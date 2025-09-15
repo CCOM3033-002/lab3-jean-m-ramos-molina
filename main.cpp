ab799526351e103b775c21403c6de44e7cbe7fde
JeanRamosUPR <jean.ramos15@upr.edu>
2025-09-15 20:57:09 +0000

    Lab3 Jean Ramos (FINAL 2da Version)

--- main.cpp
+++ main.cpp
@@ -9,14 +9,15 @@ using namespace std;
 #include <cstdlib>
 
 int main () 
-{
+    {
+
     srand(time(0));
 
     // Crear variables
 
-    int numeroAleatorio1 = rand() % (33 - 0 + 1) + 0;
-    int numeroAleatorio2 = rand() % (66 - 34 + 1) + 34;
-    int numeroAleatorio3 = rand () % (100 - 35 + 1) + 35;
+    int numeroAleatorio1 = rand() % (100 - 0 + 1) + 0;
+    int numeroAleatorio2 = rand() % (100 - 0 + 1) + 0;
+    int numeroAleatorio3 = rand () % (100 - 0 + 1) + 0;
 
     //Mostrar numeros aleatorios generados
 
@@ -29,47 +30,61 @@ int main ()
 
     if (numeroAleatorio1 >= numeroAleatorio2 && numeroAleatorio2 >= numeroAleatorio3) {
 
-    // Mostrar resultados del Caso 1
+        // Mostrar resultados del Caso 1
 
-    cout << "Orden descendiente: " << numeroAleatorio1 << " >= " << numeroAleatorio2 << " >= " << numeroAleatorio3 << endl;
+        cout << "Orden descendiente: " << numeroAleatorio1 << " >= " << numeroAleatorio2 << " >= " << numeroAleatorio3 << endl;
     } 
     
-        // Caso 2: numeroAleatorio2 mayor igual que numeroAleatorio1 y numeroAleatorio1 mayor igual que numeroAleatorio3 
-        // Orden: 2, 1, 3
+    // Caso 2: numeroAleatorio2 mayor igual que numeroAleatorio1 y numeroAleatorio1 mayor igual que numeroAleatorio3 
+    // Orden: 2, 1, 3
      
-     if  (numeroAleatorio2 >= numeroAleatorio1 && numeroAleatorio1 >= numeroAleatorio3) {
+    else if  (numeroAleatorio2 >= numeroAleatorio1 && numeroAleatorio1 >= numeroAleatorio3) {
 
         // Mostrar resultados del Caso 2
 
         cout << "Orden descendiente: " << numeroAleatorio2 << " >= " << numeroAleatorio1 << " >= " << numeroAleatorio3 << endl;
 
     }
-         // Caso 3: numeroAleatorio3 mayor igual que numeroAleatorio2 y numeroAleatorio2 mayor igual que numeroAleatorio1 
-         // Orden: 3, 2, 1
+    // Caso 3: numeroAleatorio3 mayor igual que numeroAleatorio2 y numeroAleatorio2 mayor igual que numeroAleatorio1 
+    // Orden: 3, 2, 1
 
-        if  (numeroAleatorio3 >= numeroAleatorio2 && numeroAleatorio2 >= numeroAleatorio1) {
+    else if  (numeroAleatorio3 >= numeroAleatorio2 && numeroAleatorio2 >= numeroAleatorio1) {
 
-            //Mostrar resultados del caso 3
+        //Mostrar resultados del caso 3
 
-            cout << "Orden descendiente: " << numeroAleatorio3 << " >= " << numeroAleatorio2 << " >= " << numeroAleatorio1 << endl;
-        }
+        cout << "Orden descendiente: " << numeroAleatorio3 << " >= " << numeroAleatorio2 << " >= " << numeroAleatorio1 << endl;
+    }
 
-                // Caso 4: numeroAleatorio2 mayor igual que numeroAleatorio3 y numeroAleatorio3 mayor igual que numeroAleatorio1 
-                // Orden: 2, 3, 1
+    // Caso 4: numeroAleatorio2 mayor igual que numeroAleatorio3 y numeroAleatorio3 mayor igual que numeroAleatorio1 
+    // Orden: 2, 3, 1
 
-                else {
+    else if (numeroAleatorio2 >= numeroAleatorio3 && numeroAleatorio3 >= numeroAleatorio1)   {
 
-                    // Mostrar resultados del caso 4
-                    cout << "Orden descendiente: " << numeroAleatorio2 << " >= " << numeroAleatorio3 << " >= " << numeroAleatorio1 << endl;
+        // Mostrar resultados del caso 4
+        cout << "Orden descendiente: " << numeroAleatorio2 << " >= " << numeroAleatorio3 << " >= " << numeroAleatorio1 << endl;
+    }
 
-                }
+    // Caso 5: numeroAleatorio1 mayor igual que numeroAleatorio3 y numeroAleatorio3 mayor igual que numeroAleatorio2
+    // Orden: 1, 3, 2 
 
+    else if  (numeroAleatorio1 >= numeroAleatorio3 && numeroAleatorio3 >= numeroAleatorio2)  {
 
-        
+        // Mostrar resultados del caso 5
+        cout << "Orden descendiente: " << numeroAleatorio1 << " >= " << numeroAleatorio3 << " >= " << numeroAleatorio2 << endl;
 
-                    
+    }
+
+    // Caso 6: numeroAleatorio3 mayor igual que numeroAleatorio1 y numeroAleatorio1 mayor igual que numeroAleatorio2
+    // Orden: 3, 1, 2
+
+    else    {
+
+        // Mostrar resultados del caso 6
+        cout << "Orden descendiente: " << numeroAleatorio3 << " >= " << numeroAleatorio1 << " >= " << numeroAleatorio2 << endl;
+
+    }   
     
 // FIN 
      
-    return 0;
+return 0;
 }
